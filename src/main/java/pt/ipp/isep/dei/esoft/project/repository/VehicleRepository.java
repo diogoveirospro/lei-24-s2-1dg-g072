f5550441dbd5acb149b9067659c1db3c9fee884b
tiagosampaio <1230917@isep.ipp.pt>
2024-04-24 10:13:28 +0100

    Created class Date and made changes to classes Vehicle and VehicleRepository

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/VehicleRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/VehicleRepository.java
@@ -7,11 +7,45 @@ import java.util.List;
 
 public class VehicleRepository {
 
+    /**
+     * List containing all vehicles
+     */
     private final List <Vehicle> vehicleList;
 
+    /**
+     * Repository builder
+     */
     public VehicleRepository() {
         vehicleList = new ArrayList<Vehicle>();
     }
 
+    /**
+     * Add new vehicle to list
+     * @param newVehicle
+     */
+    public void addVehicle(Vehicle newVehicle){
+        if (!validateVehicle(newVehicle)){
+            throw new IllegalArgumentException("Invalid vehicle to add");
+        }
 
+        vehicleList.add(newVehicle);
+
+    }
+
+    /**
+     * Private method to see if the vehicle is already on the list
+     * @param vehicle
+     * @return True if the vehicle isn't on the list and false if it is
+     */
+    private boolean validateVehicle(Vehicle vehicle){
+        return !vehicleList.contains(vehicle);
+    }
+
+    /**
+     * This method returns a defensive (immutable) copy of the vehicle list.
+     * @return The vehicle list
+     */
+    public List<Vehicle> getVehicleList() {
+        return (List.copyOf(vehicleList));
+    }
 }
