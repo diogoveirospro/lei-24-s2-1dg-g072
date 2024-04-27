c57cc2723a17196763feed3b42e0f17d4f3cd201
Miguel <1221119@isep.ipp.pt>
2024-04-27 23:20:47 +0100

    SkillRepository

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/SkillRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/SkillRepository.java
@@ -1,4 +1,21 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import pt.ipp.isep.dei.esoft.project.domain.Skill;
+
 public class SkillRepository {
+    private List<Skill> skills= new ArrayList<>();
+
+    public void register (Skill skill){
+        if(skill == null){
+            throw new IllegalArgumentException("Cannot be null");
+        }
+        if (skills.contains(skill)){
+            throw new IllegalArgumentException("This skill already exists"+skill.getName());
+        }
+        skills.add(skill);
+        System.out.println("Skill:"+ skill.getName());
+    }
+
 }
