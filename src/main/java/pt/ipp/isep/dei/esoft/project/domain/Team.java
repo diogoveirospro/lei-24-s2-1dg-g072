d4d3abf36dee87a0602fff2aebebb7538379cf99
Diogo Pereira <1230948@isep.ipp.pt>
2024-04-27 13:37:51 +0100

    Continuing to code the team class

--- src/main/java/pt/ipp/isep/dei/esoft/project/domain/Team.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/domain/Team.java
@@ -3,12 +3,77 @@ package pt.ipp.isep.dei.esoft.project.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a team composed of collaborators.
+ *
+ * @author Group 072 - Byte Masters
+ */
 public class Team {
 
+    /**
+     * List of collaborators representing the team.
+     */
     List<Collaborator> team;
 
+    /**
+     * Building a new team.
+     * @param members ArrayList of collaborators with the members of the team.
+     */
     public Team(ArrayList<Collaborator> members){
         team = members;
     }
 
+    /**
+     * Get the team.
+     * @return team
+     */
+    public List<Collaborator> getTeam() {
+        return team;
+    }
+
+    /**
+     * Change the team.
+     * @param team new team
+     */
+    public void setTeam(List<Collaborator> team) {
+        this.team = team;
+    }
+
+    /**
+     * Rewriting the toString method for the team class.
+     * @return String representing a team.
+     */
+    @Override
+    public String toString() {
+
+        StringBuilder team = new StringBuilder("The team members are:\n");
+
+        for (Collaborator member : this.team){
+            team.append(member);
+            team.append("\n");
+        }
+
+        return team.toString();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj){
+            return true;
+        }
+
+        if (obj == null || getClass() != obj.getClass()){
+            return false;
+        }
+
+        Team anotherTeam = (Team) obj;
+
+        for (Collaborator member : team){
+            if (!anotherTeam.getTeam().contains(member)){
+                return false;
+            }
+        }
+        return true;
+    }
 }
