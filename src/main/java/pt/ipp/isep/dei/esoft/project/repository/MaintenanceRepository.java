26f4dfe96fb47ba11ae20db445b7671db6d664cb
Diogo Veiros <diogoveirospro@gmail.com>
2024-04-27 16:58:30 +0100

    Still need to change some stuff in the MaintenanceRepository

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/MaintenanceRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/MaintenanceRepository.java
@@ -1,5 +1,7 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
+import org.w3c.dom.ls.LSException;
+import pt.ipp.isep.dei.esoft.project.domain.Job;
 import pt.ipp.isep.dei.esoft.project.domain.Maintenance;
 import pt.ipp.isep.dei.esoft.project.domain.Vehicle;
 
@@ -8,20 +10,55 @@ import java.util.List;
 
 public class MaintenanceRepository {
     /**
-     * List of all vehicles, that need maintenance.
+     * List of vehicles that will be registered for maintenance.
      *
      */
     private List<Vehicle> vehicleList;
+    /**
+     * List of vehicles that need maintenance
+     *
+     */
+    private List<Maintenance> maintenanceList;
 
-    public List<Vehicle> addVehicleMaintenance(List<Vehicle> vehicleList){
-        for (Vehicle vehicle : vehicleList){
-            Maintenance maintenance = new Maintenance(vehicle);
-            maintenance.setVehicleMaintenance(vehicle);
+    /**
+     * Get a vehicle from the repository by its details.
+     *
+     * @param vehicle that needs maintenance
+     * @return maintenance
+     */
+    public Maintenance getVehicleMaintenance(Vehicle vehicle){
+        Maintenance newMaintenance = new Maintenance(vehicle);
+        Maintenance maintenance = null;
+        if (maintenanceList.contains(newMaintenance)){
+            maintenance = maintenanceList.get(maintenanceList.indexOf(newMaintenance));
         }
-        return vehicleList;
-    }
 
-    private  boolean  validateVehicleMaintenance(Vehicle vehicle){
-        return !vehicleList.contains(vehicle);
+        if (maintenance == null){
+            throw new IllegalArgumentException("Vehicle " + vehicle + " does not exist.");
+        }
+
+        return maintenance;
+    }
+    /**
+     * Private method to see if a vehicle is already in the repository.
+     *
+     * @param maintenance: vehicle that needs maintenance to be checked
+     * @return if the vehicle is already in the repository or not
+     */
+    private  boolean  validateVehicleMaintenance(Maintenance maintenance){
+        return !maintenanceList.contains(maintenance);
+    }
+    /**
+     * Add a job to the repository.
+     * @param newMaintenance: new vehicle that needs maintenance.
+     */
+    public void addVehicleMaintenance(Maintenance newMaintenance){
+
+        if (!validateVehicleMaintenance(newMaintenance)) {
+            throw new IllegalArgumentException("Invalid vehicle that needs maintenance to add");
+        }
+
+        maintenanceList.add(newMaintenance);
+
     }
 }
