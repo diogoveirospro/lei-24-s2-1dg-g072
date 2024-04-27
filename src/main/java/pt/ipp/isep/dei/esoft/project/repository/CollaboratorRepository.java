5e8dd14b9ed0c8d96ff19b3d810f1f3963370efe
José Alves <1230630@isep.ipp.pt>
2024-04-27 11:48:46 +0100

    coded CollaboratorRepository

--- src/main/java/pt/ipp/isep/dei/esoft/project/repository/CollaboratorRepository.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/repository/CollaboratorRepository.java
@@ -1,4 +1,69 @@
 package pt.ipp.isep.dei.esoft.project.repository;
 
+import pt.ipp.isep.dei.esoft.project.domain.Collaborator;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollaboratorRepository {
+
+    /**
+     * List containing all collaborators.
+     */
+    private final List
+            <Collaborator> collaborators;
+
+    /**
+     * Repository builder.
+     */
+    public CollaboratorRepository() {
+        collaborators = new ArrayList<>();
+    }
+
+    /**
+     * Get a collaborator from the repository by their name.
+     *
+     * @param name: Collaborator name
+     * @return: collaborator
+     */
+    public Collaborator getCollaborator(String name) {
+        for (Collaborator collaborator : collaborators) {
+            if (collaborator.getName().equals(name)) {
+                return collaborator;
+            }
+        }
+        throw new IllegalArgumentException("Collaborator " + name + " does not exist.");
+    }
+
+    /**
+     * Add a collaborator to the repository.
+     *
+     * @param newCollaborator: new collaborator.
+     */
+    public void addCollaborator(Collaborator newCollaborator) {
+        if (!validateCollaborator(newCollaborator)) {
+            throw new IllegalArgumentException("Invalid collaborator to add");
+        }
+        collaborators.add(newCollaborator);
+    }
+
+    /**
+     * Private method to check if a collaborator is already in the repository.
+     *
+     * @param collaborator: collaborator to be checked
+     * @return: True if the collaborator is not yet in the repository and false otherwise.
+     */
+    private boolean validateCollaborator(Collaborator collaborator) {
+        return !collaborators.contains(collaborator);
+    }
+
+    /**
+     * This method returns a defensive (immutable) copy of the collaborator list.
+     *
+     * @return: The collaborator list.
+     */
+    public List
+            <Collaborator> getCollaborators() {
+        return List.copyOf(collaborators);
+    }
 }
