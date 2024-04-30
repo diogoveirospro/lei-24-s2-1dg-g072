510158cf404508adbfefc6f565f789fb955f539f
Diogo Veiros <diogoveirospro@gmail.com>
2024-04-30 10:20:20 +0100

    added a file from the template

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/console/DevTeamUI.java
@@ -0,0 +1,22 @@
+package pt.ipp.isep.dei.esoft.project.ui.console;
+
+/**
+ * @author Paulo Maio pam@isep.ipp.pt
+ */
+public class DevTeamUI implements Runnable {
+
+    public DevTeamUI() {
+
+    }
+
+    public void run() {
+        System.out.println("\n");
+        System.out.println("--- DEVELOPMENT TEAM -------------------");
+        System.out.println(" Diogo Veiros  1 - 1230462@isep.ipp.pt");
+        System.out.println(" Diogo Pereira 2 - 1230948@isep.ipp.pt");
+        System.out.println(" Tiago Sampaio  3 - 1230917@isep.ipp.pt");
+        System.out.println(" José Alves  4 - 1230630@isep.ipp.pt");
+        System.out.println(" Miguel Gonçalves  5 - 1221119@isep.ipp.pt");
+        System.out.println("\n");
+    }
+}
