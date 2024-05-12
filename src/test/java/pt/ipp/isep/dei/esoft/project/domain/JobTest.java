f6859efb2df3b27c2b2c7e4cb04971efb302bb14
Miguel <1221119@isep.ipp.pt>
2024-05-12 02:12:39 +0100

    Tests JobTest and SkillTest

--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/JobTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/JobTest.java
@@ -1,4 +1,52 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+import org.junit.jupiter.api.Test;
+import pt.ipp.isep.dei.esoft.project.domain.Job;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class JobTest {
+
+    @Test
+    public void testGetName() {
+        Job job = new Job("AAA");
+        assertEquals("AAA", job.getName());
+    }
+
+    @Test
+    public void testSetName() {
+        Job job = new Job("AAA");
+        job.setName("AA");
+        assertEquals("AA", job.getName());
+    }
+
+    @Test
+    public void testEquals() {
+        Job job1 = new Job("AAA");
+        Job job2 = new Job("AAA");
+        Job job3 = new Job("AA");
+
+        assertTrue(job1.equals(job2));
+        assertFalse(job1.equals(job3));
+    }
+
+    @Test
+    public void testToString() {
+        Job job = new Job("AAA");
+        assertEquals("AAA", job.toString());
+    }
+
+    @Test
+    public void testSetNameNull() {
+        assertThrows(IllegalArgumentException.class, () -> new Job(null));
+    }
+
+    @Test
+    public void testSetNameEmpty() {
+        assertThrows(IllegalArgumentException.class, () -> new Job(""));
+    }
+
+    @Test
+    public void testSetNameWhitespace() {
+        assertThrows(IllegalArgumentException.class, () -> new Job("   "));
+    }
 }
--- src/test/java/pt/ipp/isep/dei/esoft/project/domain/SkillTest.java
+++ src/test/java/pt/ipp/isep/dei/esoft/project/domain/SkillTest.java
@@ -1,4 +1,52 @@
 package pt.ipp.isep.dei.esoft.project.domain;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SkillTest {
+
+    @Test
+    public void testGetName(){
+        Skill skill1 = new Skill("AAA");
+        assertEquals("AAA",skill1.getName());
+    }
+
+    @Test
+    public void testSetName(){
+        Skill skill1 = new Skill("AAA");
+        skill1.setName("BBB");
+        assertEquals("BBB",skill1.getName());
+    }
+
+    @Test
+    public void testEquals(){
+        Skill skill1 = new Skill("AAA");
+        Skill skill2 = new Skill("AAA");
+        Skill skill3 = new Skill("BBB");
+
+        assertTrue(skill1.equals(skill2));
+        assertFalse(skill2.equals(skill3));
+    }
+
+    @Test
+    public void testToString (){
+        Skill skill1 = new Skill("AAA");
+        assertEquals("AAA",skill1.toString());
+    }
+
+    @Test
+    public void testSetNameNull (){
+        assertThrows(IllegalArgumentException.class,()-> new Skill(null));
+    }
+
+    @Test
+    public void testSetNameEmpty(){
+        assertThrows(IllegalArgumentException.class,()-> new Skill(""));
+    }
+
+    @Test
+    public void testSetNameWhiteSpace (){
+        assertThrows(IllegalArgumentException.class,()-> new Skill("   "));
+    }
+
 }
