9d79f59c7908d8822a0dc303fb4e45ec9ef6f89c
tiagosampaio <1230917@isep.ipp.pt>
2024-05-21 15:51:02 +0100

    corrections on registerVehicleUI

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/Exceptions/InvalidVehicleDataException.java
@@ -0,0 +1,9 @@
+package pt.ipp.isep.dei.esoft.project.Exceptions;
+
+public class InvalidVehicleDataException extends Exception{
+
+    public InvalidVehicleDataException(String message) {
+        super(message);
+    }
+
+}
