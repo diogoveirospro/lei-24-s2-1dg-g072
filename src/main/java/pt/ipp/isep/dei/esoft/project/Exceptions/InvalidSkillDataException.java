dc15c6276ad3630fbc72706cfb33bdc59404ad77
Diogo Pereira <1230948@isep.ipp.pt>
2024-05-21 14:13:07 +0100

    Add Exceptions

---
+++ src/main/java/pt/ipp/isep/dei/esoft/project/Exceptions/InvalidSkillDataException.java
@@ -0,0 +1,8 @@
+package pt.ipp.isep.dei.esoft.project.exceptions;
+
+public class InvalidSkillDataException extends Exception{
+
+    public InvalidSkillDataException(String message) {
+        super(message);
+    }
+}
