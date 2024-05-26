883aaf50133e6890085d0549074e095b6cd648e1
Miguel <1221119@isep.ipp.pt>
2024-05-26 20:21:27 +0100

    LoginFx

---
+++ src/main/java/applicationJavaFx/Login.java
@@ -0,0 +1,65 @@
+package applicationJavaFx;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import static javafx.application.Application.launch;
+
+public class Login extends Application{
+
+    public void start(Stage primaryStage) {
+
+        Label userLabel = new Label("Username:");
+        TextField userField = new TextField();
+
+        Label passLabel = new Label("Password:");
+        PasswordField passField = new PasswordField();
+
+        Button loginButton = new Button("Login");
+
+
+        GridPane gridPane = new GridPane();
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+
+
+        gridPane.add(userLabel, 0, 0);
+        gridPane.add(userField, 1, 0);
+        gridPane.add(passLabel, 0, 1);
+        gridPane.add(passField, 1, 1);
+        gridPane.add(loginButton, 1, 2);
+
+
+        loginButton.setOnAction(event -> {
+            String username = userField.getText();
+            String password = passField.getText();
+            if (validateLogin(username, password)) {
+                System.out.println("Login successful!");
+            } else {
+                System.out.println("Invalid username or password.");
+            }
+        });
+
+
+        Scene scene = new Scene(gridPane, 300, 200);
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private boolean validateLogin(String username, String password) {
+
+        return "admin".equals(username) && "password".equals(password);
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
+
