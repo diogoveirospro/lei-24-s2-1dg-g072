5d4341f76c4e8cd0063261434b2e9d230a2dab7b
Diogo Veiros <diogoveirospro@gmail.com>
2024-05-28 15:42:57 +0100

    ITS running

--- src/main/java/pt/ipp/isep/dei/esoft/project/Main.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/Main.java
@@ -1,25 +1,28 @@
 package pt.ipp.isep.dei.esoft.project;
 
+import javafx.stage.Stage;
 import pt.ipp.isep.dei.esoft.project.Exceptions.InvalidCollaboratorDataException;
 import pt.ipp.isep.dei.esoft.project.ui.Bootstrap;
 import pt.ipp.isep.dei.esoft.project.ui.console.menu.MainMenuUI;
+import pt.ipp.isep.dei.esoft.project.ui.gui.applicationJavaFx.Login;
+
+import static javafx.application.Application.launch;
 
 /**
  *
  *
  * @author Group 072 - Byte Masters - ISEP
  */
-public class Main {
+public class Main extends javafx.application.Application {
 
     public static void main(String[] args) throws InvalidCollaboratorDataException {
         Bootstrap bootstrap = new Bootstrap();
         bootstrap.run();
-
-        try {
-            MainMenuUI menu = new MainMenuUI();
-            menu.run();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        launch(args);
+    }
+    @Override
+    public void start(Stage primaryStage) {
+        Login loginApp = new Login();
+        loginApp.start(primaryStage);
     }
 }
