978defcae444e67c4d6e2277fb29b675fce3748d
José Alves <1230630@isep.ipp.pt>
2024-06-07 11:03:54 +0100

    US29 code

--- src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/FinishTaskController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/application/controller/FinishTaskController.java
@@ -1,4 +1,108 @@
 package pt.ipp.isep.dei.esoft.project.application.controller;
 
+import pt.ipp.isep.dei.esoft.project.Exceptions.InvalidTaskDataException;
+import pt.ipp.isep.dei.esoft.project.application.session.ApplicationSession;
+import pt.ipp.isep.dei.esoft.project.domain.*;
+import pt.ipp.isep.dei.esoft.project.repository.*;
+
+/**
+ * This class serves as the controller for the functionality of finishing a task
+ * assigned to the collaborator.
+ *
+ * @autor Group 072 - Byte Masters - ISEP
+ */
 public class FinishTaskController {
+    /**
+     * taskRepository contains all tasks
+     */
+    private TaskRepository taskRepository;
+    /**
+     * collaboratorRepository contains all collaborators
+     */
+    private CollaboratorRepository collaboratorRepository;
+    /**
+     * agenda contains all entries inside the agenda
+     */
+    private Agenda agenda;
+    private java.lang.String String;
+
+    /**
+     * Empty FinishTaskController builder.
+     */
+    public FinishTaskController() {
+        this.taskRepository = Repositories.getInstance().getTaskRepository();
+        this.collaboratorRepository = Repositories.getInstance().getCollaboratorRepository();
+        this.agenda = Repositories.getInstance().getAgenda();
+    }
+
+    /**
+     * FinishTaskController builder
+     *
+     * @param taskRepository         contains all tasks
+     * @param collaboratorRepository contains all collaborators
+     * @param agenda                 contains all tasks
+     */
+    public FinishTaskController(TaskRepository taskRepository, CollaboratorRepository collaboratorRepository, Agenda agenda) {
+        this.taskRepository = taskRepository;
+        this.collaboratorRepository = collaboratorRepository;
+        this.agenda = agenda;
+    }
+
+    /**
+     * Lets the controller get the task repository
+     *
+     * @return taskRepository
+     */
+    public TaskRepository getTaskRepository() {
+        if (taskRepository == null) {
+            Repositories repositories = Repositories.getInstance();
+
+            taskRepository = repositories.getTaskRepository();
+        }
+        return taskRepository;
+    }
+
+    /**
+     * Lets the controller get the collaborator repository
+     *
+     * @return collaboratorRepository
+     */
+    public CollaboratorRepository getCollaboratorRepository() {
+        if (collaboratorRepository == null) {
+            Repositories repositories = Repositories.getInstance();
+
+            collaboratorRepository = repositories.getCollaboratorRepository();
+        }
+        return collaboratorRepository;
+    }
+
+    /**
+     * Lets the controller get the agenda
+     *
+     * @return agenda
+     */
+    public Agenda getAgenda() {
+        if (agenda == null) {
+            Repositories repositories = Repositories.getInstance();
+
+            agenda = repositories.getAgenda();
+        }
+        return agenda;
+    }
+
+    /**
+     * Finish a task by its ID.
+     *
+     * @param taskId the ID of the task to finish
+     */
+    public void finishTask(String taskId) throws InvalidTaskDataException {
+        AgendaEntry agendaEntry = agenda.getAgendaEntry(taskId);
+        agendaEntry.taskDone();
+    }
+
+
+    private Collaborator getCollaboratorFromSession() {
+        String email = ApplicationSession.getInstance().getCurrentSession().getUserEmail();
+        return collaboratorRepository.getCollaboratorByEmail(email);
+    }
 }
--- src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/controller/Uss/FinishTaskUIController.java
+++ src/main/java/pt/ipp/isep/dei/esoft/project/ui/gui/controller/Uss/FinishTaskUIController.java
@@ -1,4 +1,51 @@
 package pt.ipp.isep.dei.esoft.project.ui.gui.controller.Uss;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import pt.ipp.isep.dei.esoft.project.application.controller.FinishTaskController;
+import pt.ipp.isep.dei.esoft.project.ui.gui.ui.Uss.FinishTaskUI;
+
+/**
+ * Controller class for managing the Finish Task UI.
+ *
+ * @author Group 072 - Byte Masters - ISEP
+ */
 public class FinishTaskUIController {
+
+    private FinishTaskController finishTaskController;
+    private FinishTaskUI finishTaskUI;
+
+    @FXML
+    private TextField taskIdField;
+
+    @FXML
+    private Button finishTaskButton;
+
+    public FinishTaskUIController() {
+        finishTaskController = new FinishTaskController();
+    }
+
+    public void setFinishTaskUI(FinishTaskUI finishTaskUI) {
+        this.finishTaskUI = finishTaskUI;
+    }
+
+    @FXML
+    public void finishTask() {
+        String taskId = taskIdField.getText();
+        try {
+            finishTaskController.finishTask(taskId);
+            showAlert(Alert.AlertType.INFORMATION, "Success", "Task marked as completed successfully.");
+        } catch (Exception e) {
+            showAlert(Alert.AlertType.ERROR, "Error", e.getMessage());
+        }
+    }
+
+    private void showAlert(Alert.AlertType alertType, String title, String message) {
+        Alert alert = new Alert(alertType);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
 }
